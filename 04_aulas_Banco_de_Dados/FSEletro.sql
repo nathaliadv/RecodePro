2ce75ec77657accd70b2283c6421d4c10fb92223
nathaliadv <nathaliadantasv@gmail.com>
2021-02-01 11:27:08 -0300

    alteração no script

--- 04_aulas_Banco_de_Dados/FSEletro.sql
+++ 04_aulas_Banco_de_Dados/FSEletro.sql
@@ -1,27 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5deb2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 25/10/2020 às 18:03
--- Versão do servidor: 8.0.21-0ubuntu0.20.04.4
--- Versão do PHP: 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Banco de dados: `FSEletro`
 --
 
+CREATE SCHEMA FSEletro;
+
+USE FSEletro;
 -- --------------------------------------------------------
 
 --
